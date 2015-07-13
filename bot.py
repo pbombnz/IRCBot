cd54dc67d56397d60c48b69783c44df6ee98b555
Prashant B <prashant_bhikhu@hotmail.com>
2015-07-14 00:58:09 +1200

    changed "kobra" library to "irclib" library

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import kobra
+import irclib
 
 server = "irc.sacnr.com"
 port = 6697
@@ -8,6 +8,6 @@ nick_name = "Kobra"
 real_name = "Kobra - IRC Python Framework (By PBombNZ)"
 password = str()
 
-irc = kobra.Bot(server, port, True, nick_name, user_name, real_name, password)
+irc = irclib.Bot(server, port, True, nick_name, user_name, real_name, password)
 irc.connect()
 irc.join('#PBomb')
