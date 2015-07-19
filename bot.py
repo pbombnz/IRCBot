85a6354b9174a6503472257b226fc90dcdc8d91d
Prashant B <prashant_bhikhu@hotmail.com>
2015-07-19 16:36:51 +1200

    Changed parameters to be more generic

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import irclib
 server = "irc.sacnr.com"
 port = 6697
 ssl = True
-user_name = "K"
-nick_name = "Kobra"
-real_name = "Kobra - IRC Python Framework (By PBombNZ)"
+user_name = "PythonBot"
+nick_name = "PythonBot"
+real_name = "IRC Python Framework (By Prashant B.)"
 password = str()
 
 irc = irclib.Bot(server, port, True, nick_name, user_name, real_name, password)
