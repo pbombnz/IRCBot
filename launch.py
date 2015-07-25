ae12f02b11a4843f2b78c3e5bf70043600aa60e4
Prashant B <prashant_bhikhu@hotmail.com>
2015-07-25 18:15:24 +1200

    Renaming files to avoid future conflict.

--- launch.py
+++ launch.py
@@ -1,4 +1,4 @@
-from irclib import bot
+from irclib import Bot
 
 server = "irc.sacnr.com"
 port = 6697
@@ -8,6 +8,6 @@ nick_name = "PythonBot"
 real_name = "IRC Python Framework (By Prashant B.)"
 password = str()
 
-irc = bot.IRCBot(server, port, True, nick_name, user_name, real_name, password)
+irc = Bot.IRCBot(server, port, True, nick_name, user_name, real_name, password)
 irc.connect()
 irc.join('#PBomb')
