dae2d6ddc45eebeff16cb141969d730f2afa1339
Prashant B <prashant_bhikhu@hotmail.com>
2015-07-25 18:18:14 +1200

    Seperated IRCDict into its own python file

---
+++ irclib/dict.py
@@ -0,0 +1,14 @@
+class Dict(dict):
+    """
+        An Internal class that overrides certain methods of a standard dictionary
+        object in order for the keys to be case insensitive.
+    """
+
+    def __contains__(self, key):
+        return dict.__contains__(self, key.lower())
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key.lower())
+
+    def __setitem__(self, key, value):
+        return dict.__setitem__(self, key.lower(), value)
